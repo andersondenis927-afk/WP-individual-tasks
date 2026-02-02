c6c23c3da22df536ad316bcbea6b6abd7a33539e
andersondenis927-afk <andersondenis927@gmail.com>
2026-02-02 15:17:08 +0200

    ex1

--- src/ex1.php
+++ src/ex1.php
@@ -1,3 +1,47 @@
+<html>
+    <title>
+         Exercise 1: Getting Started with PHP - YourFirstName 
+    </title>
 <?php
-echo "Hello world! My name is \"David\" ";
+echo "<h3>Hello world! My name is \"David\" </h3> ";
 ?>
+
+<?php
+$title = "PHP is interesting.";
+?>
+
+<h4><?php echo $title; ?></h4>
+
+<?php
+$g1 = 5;
+$g2 = 4;
+$g3 = 5;
+?>
+
+<table border="1">
+    <tr>
+        <th>S.n.</th>
+        <th>Name</th>
+        <th>Grade</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>John</td>
+        <td><?php echo $g1; ?></td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td>Alice</td>
+        <td><?php echo $g2; ?></td>
+    </tr>
+    <tr>
+        <td>3</td>
+        <td>Bob</td>
+        <td><?php echo $g3; ?></td>
+    </tr>
+</table>
+
+<img src="image.png">
+
+</html>
+
