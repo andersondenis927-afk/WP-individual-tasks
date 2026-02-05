e07ceabddd4b3346b65aaecfd54e1a4a003e7bc6
andersondenis927-afk <andersondenis927@gmail.com>
2026-02-05 12:25:35 +0200

    ex2

---
+++ src/footer.php
@@ -0,0 +1,67 @@
+<footer style="
+    padding:50px 0;
+    background:lightgray;
+    font-family:'Poppins', sans-serif;
+">
+    <div style="
+        width:1200px;
+        margin:0 auto;
+        display:flex;
+        justify-content:space-between;
+    ">
+        <div>
+            <span style="
+                text-transform:uppercase;
+                font-size:29px;
+                font-weight:800;
+            ">
+                LAST MEAL
+            </span>
+        </div>
+
+        <div>
+            <h3 style="
+                font-weight:500;
+                font-size:17px;
+                margin-bottom:10px;
+            ">
+                About us
+            </h3>
+            <ul style="list-style:none;padding:0;margin:0;">
+                <li style="margin-top:7px;opacity:0.8;">
+                    <a href="main.html" style="text-decoration:none;color:black;">Home</a>
+                </li>
+                <li style="margin-top:7px;opacity:0.8;">
+                    <a href="recipes.html" style="text-decoration:none;color:black;">Recipes</a>
+                </li>
+                <li style="margin-top:7px;opacity:0.8;">
+                    <a href="poisonrecipe.html" style="text-decoration:none;color:black;">Share your poison</a>
+                </li>
+                <li style="margin-top:7px;opacity:0.8;">
+                    <a href="account.html" style="text-decoration:none;color:black;">Witch account</a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 style="
+                font-weight:500;
+                font-size:17px;
+                margin-bottom:10px;
+            ">
+                Contact us
+            </h3>
+            <ul style="list-style:none;padding:0;margin:0;">
+                <li style="margin-top:7px;opacity:0.8;">
+                    denis.anderson@student.hamk.fi
+                </li>
+                <li style="margin-top:7px;opacity:0.8;">
+                    martin.pevgonen@student.hamk.fi
+                </li>
+                <li style="margin-top:7px;opacity:0.8;">
+                    jekaterina.zasijenko@student.hamk.fi
+                </li>
+            </ul>
+        </div>
+    </div>
+</footer>
---
+++ src/header.php
@@ -0,0 +1,60 @@
+<header style="
+    padding:50px 0;
+    width:1200px;
+    margin:0 auto;
+    font-family:'Poppins', sans-serif;
+">
+    <span style="
+        text-transform:uppercase;
+        font-size:29px;
+        font-weight:800;
+    ">
+        <a href="main.html" style="text-decoration:none;color:black;">Last Meal</a>
+    </span>
+
+    <nav style="float:right;width:50%;">
+        <ul style="
+            list-style:none;
+            display:flex;
+            justify-content:space-between;
+            margin:0;
+            padding:0;
+        ">
+            <li>
+                <a href="main.html" style="text-decoration:none;color:black;">
+                    Home
+                </a>
+            </li>
+
+            <li style="position:relative;">
+                <a href="recipes.html" style="text-decoration:none;color:black;">
+                    Recipes
+                </a>
+                <span style="
+                    display:block;
+                    width:40px;
+                    height:5px;
+                    background:lawngreen;
+                    border-radius:10px;
+                    position:absolute;
+                    top:30px;
+                    left:-15px;
+                "></span>
+            </li>
+
+            <li>
+                <a href="poisonrecipe.html" style="text-decoration:none;color:black;">
+                    Share your poison
+                </a>
+            </li>
+
+            <li>
+                <a href="account.html" style="text-decoration:none;color:black;">
+                    Witch account
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+    <div style="clear:both;"></div>
+</header>
